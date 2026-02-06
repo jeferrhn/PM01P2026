7c078d018f1d4e8a8b068ebd2e47d192d92f5c37
Jeferson Martinez <jeferr.mar.uth@gmail.com>
2026-02-05 20:24:47 -0600

    Cambios pantalla inicio 24-01-2026

--- app/src/main/java/com/example/pm01p2026/MainActivity.java
+++ app/src/main/java/com/example/pm01p2026/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.pm01p2026;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     EditText nombres, apellidos, edad, correo;
     ImageView foto;
-    Button agregar;
+    Button agregar, btnfoto;
+
+    static final int REQUEST_CAMARA_PERMISO = 230;
+    static final int REQUEST_IMAGE_CAPTURE = 191;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,18 +49,73 @@ public class MainActivity extends AppCompatActivity {
         edad = (EditText) findViewById(R.id.Edad);
         correo = (EditText) findViewById(R.id.Correo);
         agregar = (Button) findViewById(R.id.button);
+        btnfoto = (Button) findViewById(R.id.btnfoto);
 
         agregar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 //Toast.makeText(getApplicationContext(),"Hola",Toast.LENGTH_LONG).show();
-
                 AddPerson();
-
-
             }
         });
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TomarFoto();
+            }
+        });
+
+
     }
+    private void TomarFoto()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMARA_PERMISO);
+        }
+        else
+        {
+            AbrirCamara();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CAMARA_PERMISO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                AbrirCamara();
+            } else {
+                Toast.makeText(this, "Permiso Denegado", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void AbrirCamara()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap img = (Bitmap) extras.get("data"); // miniatura
+                foto.setImageBitmap(img);
+            }
+        }
+    }
+
     private void AddPerson()
     {
         try {
@@ -82,8 +151,6 @@ public class MainActivity extends AppCompatActivity {
         edad.setText("");
         correo.setText("");
         nombres.requestFocus();
-
-
 
     };
 }
--- app/src/main/java/com/example/pm01p2026/Models/Personas.java
+++ app/src/main/java/com/example/pm01p2026/Models/Personas.java
@@ -21,39 +21,30 @@ public class Personas
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getNombres() {
         return nombres;
     }
-
     public void setNombres(String nombres) {
         this.nombres = nombres;
     }
-
     public String getApellido() {
         return apellido;
     }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
     public Integer getEdad() {
         return edad;
     }
-
     public void setEdad(Integer edad) {
         this.edad = edad;
     }
-
     public String getCorreo() {
         return correo;
     }
-
     public void setCorreo(String correo) {
         this.correo = correo;
     }
